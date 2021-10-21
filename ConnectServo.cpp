2ef43b97c140ad6d1b6d760344c33fbf109174e2
jjsanderson <jonathan@quernstone.com>
2021-10-21 14:31:53 +0100

    First working queue implementation

--- ConnectServo.cpp
+++ ConnectServo.cpp
@@ -1,5 +1,22 @@
 #include <ConnectServo.h>
 
-ConnectServo::ConnectServo() {
+// ServoQueueItem initializer
+ServoQueueItem::ServoQueueItem() {
 
-}
+};
+
+// Initialize the servo object, passing an initializer list to the cppQueue object
+// See http://arduinoetcetera.blogspot.com/2011/01/classes-within-classes-initialiser.html
+ConnectServo::ConnectServo() : _servoQueue(sizeof(ServoQueueItem), QUEUE_SIZE_ITEMS, IMPLEMENTATION) {
+
+};
+
+void ConnectServo::enqueue(ServoQueueItem item) {
+  _servoQueue.push(&item);
+};
+
+ServoQueueItem ConnectServo::dequeue() {
+    ServoQueueItem _item;
+    _servoQueue.pop(&_item);
+    return _item;
+};
--- ConnectServo.h
+++ ConnectServo.h
@@ -1,7 +1,39 @@
 #include <Arduino.h>
 #include <ServoEasing.h>
+#include <cppQueue.h>
+
+#define IMPLEMENTATION FIFO
+#define QUEUE_SIZE_ITEMS 20
+
+// Generic function pointer. Note variable-length argument list
+typedef void (*GenericFP)(void *, ...);
+
+// Servo queue item type
+// typedef struct {
+//     GenericFP call;             // eg. startEaseTo, moveTo/write, waitForServo
+//     int param1;                 // Target position or (for waitForServo) servo to wait on.
+//     const char * animationType; // eg. 'EASE_CUBIC_IN_OUT'
+//     int servoSpeed;             // Slew rate (perscentage: map to 0-255. Servo theoretical max is around 200 degrees/sec, ~80%)
+// } servoQueueItem;
+
+class ServoQueueItem {
+    public:
+        ServoQueueItem();
+        GenericFP call;             // eg. startEaseTo, moveTo/write, waitForServo
+        int param1;                 // Target position or (for waitForServo) servo to wait on.
+        const char * animationType; // eg. 'EASE_CUBIC_IN_OUT'
+        int servoSpeed;             // Slew rate (perscentage: map to 0-255. Servo theoretical max is around 200 degrees/sec, ~80%)
+};
 
 class ConnectServo : public ServoEasing {
     public:
         ConnectServo();
+        void enqueue(ServoQueueItem item);
+        ServoQueueItem dequeue();
+        // cppQueue servoQueue;
+    private:
+        cppQueue _servoQueue;
+        ServoQueueItem _queueItem1;
+        ServoQueueItem _queueItem2;
+
 };
